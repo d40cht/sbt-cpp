ac855b2bccfc8839810ed02b17a9abf7448efdc8
Alex Wilson <alex.wilson@pobox.com>
2013-04-21 20:06:40 +0100

    Fiddles with visual studio.

---
+++ experiments/vs2010/vcvars.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ SDKs/Windows/v7.0A/Bin:/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 10.0/VC/Bin:/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 10.0/Common7/IDE
+export INCLUDE="c:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/Include;c:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/include"
+export LIB="c:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/Lib;c:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/lib"
+
