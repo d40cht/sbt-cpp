b669fde577fcb56ca506047cdab54edcb86929ef
Alex Wilson <alex.wilson@navetas.com>
2013-05-10 22:06:21 +0100

    Tidying up test script.

--- test/various/build.sh
+++ test/various/build.sh
@@ -4,8 +4,8 @@ set -e
 
 rm -rf target
 
-../../sbt "native-build-configuration Gcc_LinuxPC_Debug" native-clean-all clean compile test
-../../sbt "native-build-configuration Gcc_LinuxPC_Release" clean compile test
-../../sbt "native-build-configuration Clang_LinuxPC_Debug" clean compile test
-../../sbt "native-build-configuration Clang_LinuxPC_Release" clean compile test
+../../sbt "native-build-configuration Gcc_LinuxPC_Debug" compile test
+../../sbt "native-build-configuration Gcc_LinuxPC_Release" compile test
+../../sbt "native-build-configuration Clang_LinuxPC_Debug" compile test
+../../sbt "native-build-configuration Clang_LinuxPC_Release" compile test
 
