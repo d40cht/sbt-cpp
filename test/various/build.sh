f7d6ab0afcb5e54b44c3119416a2dddad880e9e8
Alex Wilson <alex.wilson@navetas.com>
2013-10-10 20:54:28 +0100

    Temporarily disable some Clang tests as the Travis combination of 3.2 and gcc 4.7 does not play very nicely.

--- test/various/build.sh
+++ test/various/build.sh
@@ -6,6 +6,6 @@ rm -rf target
 
 ../../sbt "nativeBuildConfiguration Gcc_LinuxPC_Debug" compile test
 ../../sbt "nativeBuildConfiguration Gcc_LinuxPC_Release" compile test
-../../sbt "nativeBuildConfiguration Clang_LinuxPC_Debug" compile test
-../../sbt "nativeBuildConfiguration Clang_LinuxPC_Release" compile test
+#../../sbt "nativeBuildConfiguration Clang_LinuxPC_Debug" compile test
+#../../sbt "nativeBuildConfiguration Clang_LinuxPC_Release" compile test
 
