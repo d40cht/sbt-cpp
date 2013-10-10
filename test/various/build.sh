a422bfa6d0351829b0125419f2c44b523313b5ec
Alex Wilson <alex.wilson@navetas.com>
2013-10-10 16:48:36 +0100

    Fix up test build script. Still not working.

--- test/various/build.sh
+++ test/various/build.sh
@@ -4,8 +4,8 @@ set -e
 
 rm -rf target
 
-../../sbt "native-build-configuration Gcc_LinuxPC_Debug" compile test
-../../sbt "native-build-configuration Gcc_LinuxPC_Release" compile test
-../../sbt "native-build-configuration Clang_LinuxPC_Debug" compile test
-../../sbt "native-build-configuration Clang_LinuxPC_Release" compile test
+../../sbt "nativeBuildConfiguration Gcc_LinuxPC_Debug" compile test
+../../sbt "nativeBuildConfiguration Gcc_LinuxPC_Release" compile test
+../../sbt "nativeBuildConfiguration Clang_LinuxPC_Debug" compile test
+../../sbt "nativeBuildConfiguration Clang_LinuxPC_Release" compile test
 
